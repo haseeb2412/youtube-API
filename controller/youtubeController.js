caa4cc52eb4447e0f972ddc89a92c22d68414448
[Muhammad Haseeb] <[haseeb241202@gmail.com]>
2023-11-20 09:14:18 -0800

    almost completed

--- controller/youtubeController.js
+++ controller/youtubeController.js
@@ -1,20 +1,34 @@
 const Youtube = require("../models/model")
 
 const getYoutube =async(req,res)=>{
-    const {channel,sort} = req.query
+    const {channel,sort,timestamp} = req.query
     const queryObject = {};
 
     if(channel){
         queryObject.channel = channel;
     }
+    if(timestamp){
+        queryObject.timestamp = timestamp;
+    }
 
+    let apiData = Youtube.find(queryObject);
     
+
     if(sort){
         let sortFix = sort.replace(","," ");
         apiData = apiData.sort(sortFix);
     }
 
-    const  mydata = await Youtube.find(queryObject);
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
+    let skip =(page-1)*limit;
+    apiData  = apiData.skip(skip).limit(limit);
+
+        console.log(queryObject);
+        const mydata = await Youtube.find(req.query).skip(2);
+
+    // const  mydata = await Youtube.find(queryObject);
     res.status(200).json({mydata, nbHits : mydata.length});
 
 
